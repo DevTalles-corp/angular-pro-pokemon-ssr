dc5db626c5625ce4482cbfabaa45fe9de2a921da
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-25 11:24:23 -0400

    chore: Update Angular and platform dependencies

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,8 +1,10 @@
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
+  PLATFORM_ID,
 } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 
@@ -16,8 +18,15 @@ import { Title, Meta } from '@angular/platform-browser';
 export default class PricingPageComponent implements OnInit {
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    // console.log({ hola: 'mundo' });
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({
       name: 'description',
