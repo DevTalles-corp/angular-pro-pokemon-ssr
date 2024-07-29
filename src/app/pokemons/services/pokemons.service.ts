e9b85bb2815fed8590e656821a35ac0e0577cec3
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-29 14:09:43 -0400

    chore: Update Pokemon service with error handling for HTTP requests

--- src/app/pokemons/services/pokemons.service.ts
+++ src/app/pokemons/services/pokemons.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { PokeAPIResponse, Pokemon, SimplePokemon } from '../interfaces';
 
 @Injectable({
@@ -39,6 +39,20 @@ export class PokemonsService {
   }
 
   public loadPokemon(id: string) {
-    return this.http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    return this.http
+      .get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.log('An error occurred:', error.error);
+    } else {
+      console.log(`Backend returned code ${error.status}, body:`, error.error);
+    }
+
+    const errorMessage = error.error ?? 'An error occurred';
+
+    return throwError(() => new Error(errorMessage));
   }
 }
