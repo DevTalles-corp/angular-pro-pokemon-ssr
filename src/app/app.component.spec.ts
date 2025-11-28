a76582ea22f645171c9cc48452509ad9267db570
Fernando Herrera <fernando.herrera85@gmail.com>
2025-11-28 15:42:28 -0500

    chore: Remove unused Jasmine dependencies from package.json and package-lock.json, update TypeScript configuration for references, and adjust test file structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      // providers: [provideRouter([])],
     }).compileComponents();
   });
 
@@ -22,12 +20,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('pokemon-ssr');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain(
-  //     'Hello, pokemon-ssr'
-  //   );
-  // });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, pokemon-ssr'
+    );
+  });
 });
