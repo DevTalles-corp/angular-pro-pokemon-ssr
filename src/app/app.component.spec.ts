d92b3436995044d0a1099dcfea3e75cdb04fdbf2
Fernando Herrera <fernando.herrera85@gmail.com>
2025-11-29 10:39:45 -0500

    test: Refactor AppComponent tests to use MockNavbarComponent and add CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,70 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-navbar',
+  template: `
+    <nav class="test-class">
+      <a href="test-link">Test Link</a>
+    </nav>
+  `,
+})
+class MockNavbarComponent {}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+    // ! #1
+    // await TestBed.configureTestingModule({
+    //   imports: [AppComponent],
+    //   providers: [provideRouter([])],
+    // }).compileComponents();
+
+    // ! #2
+    // await TestBed.configureTestingModule({
+    //   imports: [AppComponent],
+    //   providers: [provideRouter([])],
+    // })
+    //   .overrideComponent(AppComponent, {
+    //     add: {
+    //       imports: [MockNavbarComponent],
+    //     },
+    //     remove: {
+    //       imports: [NavbarComponent],
+    //     },
+    //   })
+    //   .compileComponents();
+
+    // ! #3
+    TestBed.overrideComponent(AppComponent, {
+      set: {
+        imports: [MockNavbarComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      },
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    // const fixture = TestBed.createComponent(AppComponent);
+    // const app = fixture.componentInstance;
+
+    // console.log(fixture.nativeElement.innerHTML);
+
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pokemon-ssr' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokemon-ssr');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should render the navbar and router-outlet', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, pokemon-ssr'
-    );
+
+    expect(compiled.querySelector('app-navbar')).toBeTruthy();
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,9 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-    selector: 'app-navbar',
-    imports: [RouterLink, RouterLinkActive],
-    templateUrl: './navbar.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {}
